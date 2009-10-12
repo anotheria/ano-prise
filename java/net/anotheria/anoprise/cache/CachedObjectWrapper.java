8688d59b517836559cb2b6a5f9c37f304aaaf862
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-10-12 13:24:26 +0000

    header

--- java/net/anotheria/anoprise/cache/CachedObjectWrapper.java
+++ java/net/anotheria/anoprise/cache/CachedObjectWrapper.java
@@ -1,20 +1,3 @@
-/* ------------------------------------------------------------------------- *
-$Source$
-$Author$
-$Date$
-$Revision$
-
-
-Copyright 2004-2005 by FriendScout24 GmbH, Munich, Germany.
-All rights reserved.
-
-This software is the confidential and proprietary information
-of FriendScout24 GmbH. ("Confidential Information").  You
-shall not disclose such Confidential Information and shall use
-it only in accordance with the terms of the license agreement
-you entered into with FriendScout24 GmbH.
-See www.friendscout24.de for details.
-** ------------------------------------------------------------------------- */
 package net.anotheria.anoprise.cache;
 
 public class CachedObjectWrapper<T> {
@@ -42,13 +25,3 @@ public class CachedObjectWrapper<T> {
 		this.timestamp = timestamp;
 	}
 }
-
-/* ------------------------------------------------------------------------- *
- * $Log$
- * Revision 1.2  2006-11-15 13:00:49  lrosenberg
- * *** empty log message ***
- *
- * Revision 1.1  2006/02/07 14:53:03  lrosenberg
- * *** empty log message ***
- *
- */
