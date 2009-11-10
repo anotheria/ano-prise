2a525e430b544b58182d226ec40b312b4ad42df2
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-11-10 16:26:38 +0000

    added metafactory test suite

---
+++ test/junit/net/anotheria/anoprise/metafactory/MetaFactoryTestSuite.java
@@ -0,0 +1,11 @@
+package net.anotheria.anoprise.metafactory;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value=Suite.class)
+@SuiteClasses(value={MetaFactoryTest.class, ConfigurableResolverTest.class} )
+public class MetaFactoryTestSuite {
+
+}
