c90081e59ec7b1ff11bdb69f1b0366307541d355
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-11-10 16:29:53 +0000

    fix attempt

--- test/junit/net/anotheria/anoprise/metafactory/MetaFactoryTestSuite.java
+++ test/junit/net/anotheria/anoprise/metafactory/MetaFactoryTestSuite.java
@@ -1,5 +1,8 @@
 package net.anotheria.anoprise.metafactory;
 
+import org.configureme.ConfigurationManager;
+import org.configureme.environments.DynamicEnvironment;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -7,5 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(value=Suite.class)
 @SuiteClasses(value={MetaFactoryTest.class, ConfigurableResolverTest.class} )
 public class MetaFactoryTestSuite {
-
+	@BeforeClass public static void setConfigureMe(){
+		ConfigurationManager.INSTANCE.setDefaultEnvironment(new DynamicEnvironment("test", "junut"));
+	}
 }
