da638ac0417a0052d3a6a9035ca7e88867e1957b
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-11-23 17:23:00 +0000

    added bounded cache to cache suite

--- test/junit/net/anotheria/anoprise/cache/CacheTestSuite.java
+++ test/junit/net/anotheria/anoprise/cache/CacheTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
 @SuiteClasses(value={RoundRobinSoftReferenceExperimentalCacheTest.class,RoundRobinSoftReferenceCacheTest.class,RoundRobinHardwiredCacheTest.class,
-		ExpiringCacheTest.class, TestConfigurationReading.class, ConfigurableRoundRobinHardwiredCacheTest.class} )
+		ExpiringCacheTest.class, TestConfigurationReading.class, ConfigurableRoundRobinHardwiredCacheTest.class, BoundedHardwiredCacheTester.class} )
 
 public class CacheTestSuite {
 	public static final int START_SIZE = 3000;
