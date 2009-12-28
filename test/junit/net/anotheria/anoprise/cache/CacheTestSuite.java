bac38b27d92d7615f574345e308460fa8e2d085d
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-12-28 23:53:08 +0000

    reduced repetition count to speed up tests

--- test/junit/net/anotheria/anoprise/cache/CacheTestSuite.java
+++ test/junit/net/anotheria/anoprise/cache/CacheTestSuite.java
@@ -11,7 +11,7 @@ public class CacheTestSuite {
 	public static final int START_SIZE = 3000;
 	public static final int MAX_SIZE = 5000;
 	
-	public static final int REPETITION_COUNT = 100000;
+	public static final int REPETITION_COUNT = 10000;
 	public static final int PARALLEL_THREAD_COUNT = 5;
 }
  
