27a92c2212449981fb40eebe92e49a9acfa7825a
Leon Rosenberg <leon.rosenberg@anotheria.net>
2016-08-21 23:44:31 -0400

    PMD warnings, remove unused public keyword

--- src/main/java/net/anotheria/anoprise/eventservice/EventChannel.java
+++ src/main/java/net/anotheria/anoprise/eventservice/EventChannel.java
@@ -14,26 +14,26 @@ public interface EventChannel {
 	 *
 	 * @param e a {@link net.anotheria.anoprise.eventservice.Event} object.
 	 */
-	public void push(Event e);
+	void push(Event e);
 	
 	/**
 	 * Adds a consumer to this channel.
 	 *
 	 * @param consumer consumer to add.
 	 */
-	public void addConsumer(EventServiceConsumer consumer);
+	void addConsumer(EventServiceConsumer consumer);
 	
 	/**
 	 * Removes a consumer from this channel.
 	 *
 	 * @param consumer to remove.
 	 */
-	public void removeConsumer(EventServiceConsumer consumer);
+	void removeConsumer(EventServiceConsumer consumer);
 	
 	/**
 	 * Returns the name of this channel.
 	 *
 	 * @return the name of the channel.
 	 */
-	public String getName(); 
+	String getName();
 }
