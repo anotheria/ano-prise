06822836830c1d0f027c638c74562e28d2d88a1c
asamoilich <asamoilich@anotheria.net>
2023-11-01 16:16:33 +0200

    migrate to java 11

--- src/test/java/net/anotheria/anoprise/metafactory/ConfigurableResolverTest.java
+++ src/test/java/net/anotheria/anoprise/metafactory/ConfigurableResolverTest.java
@@ -4,7 +4,9 @@ import org.configureme.ConfigurationManager;
 import org.configureme.environments.DynamicEnvironment;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static junit.framework.Assert.*;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
 
 public class ConfigurableResolverTest {
 	
